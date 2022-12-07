7883c30227c48b3a032e86a06edfe15d9ac03a7a
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-12-07 22:21:20 +0530

    Create python_06_user_input.py

---
+++ python_06_user_input.py
@@ -0,0 +1,11 @@
+name = input("enter your name :")
+print(name)
+
+a = input("enter the first number :")
+b = input("enter the second number :")
+
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(a) / int(b))
+print(int(a) % int(b))
