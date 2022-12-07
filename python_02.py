f174aba21bbac0b972dd9332a560e01f4bc93d33
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-12-07 22:01:42 +0530

    Create python_02.py

---
+++ python_02.py
@@ -0,0 +1,10 @@
+# commenting code in python
+# printing hello world
+print("hello world")  #printed
+
+# escape sequences
+print("welcome to python\n programming")
+
+print("welcome to \"python\" programming")
+
+print("hii", "hello", 1, 2, "\n", sep="-", end="bii")
