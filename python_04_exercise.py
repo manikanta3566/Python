825ed1dc43ff0a4555b05c46697c93c3ee84a47d
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-12-07 22:04:15 +0530

    Create python_04_exercise.py

---
+++ python_04_exercise.py
@@ -0,0 +1,7 @@
+a = 30
+b = 20
+print("addition of a and b is ", a + b)
+print("subtraction of a and b is ", a - b)
+print("division of a and b is ", a / b)
+print("multiplication of a and b is ", a * b)
+print("modulus of a and b is ", a % b)
