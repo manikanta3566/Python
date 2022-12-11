312dcefda01f4d518d4a9393bdaa46a883afc3ac
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-12-11 19:59:48 +0530

    Create python_09_string_methods.py

---
+++ python_09_string_methods.py
@@ -0,0 +1,45 @@
+name="!!sachin !!!!!"
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.rstrip("!"))
+print(name.replace("!","."))
+print(name.split(" "))
+
+heading="hellowelcome"
+print(heading.capitalize())
+
+print(heading.center(50))
+
+print(name.count("!"))
+
+print(name.endswith("!!!"))
+
+print(name.endswith("in",0,8))
+
+print(name.find("in"))
+
+print(name.find("w"))
+
+print(name.index("in"))
+
+# print(name.index("o"))
+
+print(name.isalnum())
+print(heading.isalpha())
+
+print(name.islower())
+
+print(name.isprintable())
+
+print(name.isspace())
+
+print(name.istitle())
+
+print(name.isupper())
+
+print(name.startswith("!"))
+
+print(name.title())
+
+print(name.swapcase())
