8d0b9ac21624a21e274d3ce05e539d4d7bd27c1d
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-12-14 23:01:20 +0530

    Create python_12_for_loop.py

---
+++ python_12_for_loop.py
@@ -0,0 +1,21 @@
+name="john"
+
+for s in name:
+  print(s)
+
+colors=["red","blue","green","white"]
+for color in colors:
+  print(color)
+
+for n in range(20):
+  print(n)
+
+print("-------")
+
+for i in range(1,30):
+  print(i)
+
+print("-------")
+
+for j in range(1,10,2):
+  print(j)
