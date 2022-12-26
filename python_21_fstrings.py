a117ef8d11c34e7278b6139fe92c7cd95f6007af
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-12-26 22:24:01 +0530

    Create python_21_fstrings.py

---
+++ python_21_fstrings.py
@@ -0,0 +1,14 @@
+string = "my name is {} and i am from {}"
+country = "india"
+name = "ashik"
+print(string.format(name, country))
+
+#fstring
+print(f"my name is {name} and i am from {country}")
+
+print(f"my name is {{name}} and i am from {{country}}")
+
+price = 40.90000
+print(f"price is {price:.2F}")
+
+print(f"{2+3}")
