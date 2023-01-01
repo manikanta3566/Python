00bbe6c78f380848697b3aa4a128206123aabdad
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2023-01-01 19:39:22 +0530

    Create python_27_for_with_else.py

---
+++ python_27_for_with_else.py
@@ -0,0 +1,12 @@
+for i in range(5):
+  print(i)
+else:
+  print("executed else block")
+
+#with break
+for j in range(5):
+  print(j)
+  if (j == 2):
+    break
+else:
+  print("else block executed")
