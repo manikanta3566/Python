947cf33fe34b937e36b4b98abd692383ac405194
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2023-01-01 22:04:45 +0530

    Update python_27_dictionary_method.py

--- python_27_dictionary_method.py
+++ python_27_dictionary_method.py
@@ -1,1 +1,13 @@
 
+emp1 = {1: "john", 2: "mike"}
+emp2 = {3: "jim", 4: "charles"}
+
+# emp1.clear()
+emp1.update(emp2)
+# del emp1[3]
+print(emp1)
+# del emp1
+
+emp1.pop(1)
+emp1.popitem()
+print(emp1)
